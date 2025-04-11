119dc6d310aefdc73e3b3a4f7370fa3ea791dc6b
elena_dobruk <elena.dobruk@yandex.ru>
2025-04-11 13:05:08 +0300

    fix function-based authentication check

--- urlshortener/links/views.py
+++ urlshortener/links/views.py
@@ -33,7 +33,7 @@ def register_user(request):
 #         return redirect("homepage")
 #    
 #     elif request.method == "POST":
-#         form = AuthenticationForm(request.POST)
+#         form = AuthenticationForm(request, request.POST)
 #         if form.is_valid():
 #             username = form.cleaned_data.get('username')
 #             password = form.cleaned_data.get('password')
