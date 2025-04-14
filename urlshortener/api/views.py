0e48e2ecc9cf8fb121de0ad7c10018c6ed050257
elena_dobruk <elena.dobruk@yandex.ru>
2025-04-14 16:23:16 +0300

    add index and url_shorten views

--- urlshortener/api/views.py
+++ urlshortener/api/views.py
@@ -1,5 +1,20 @@
-from rest_framework import viewsets
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+
+from api.serializers import URLSerializer
+from links.models import URL
 
 
-class URLViewSet(viewsets.ModelViewSet):
-    pass
+class URLListView(generics.ListAPIView):
+    serializer_class = URLSerializer
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        current_user = self.request.user
+        return URL.objects.filter(user=current_user)
+
+
+class URLCreateView(generics.CreateAPIView):
+    queryset = URL.objects.all()
+    serializer_class = URLSerializer
+    permission_classes = [IsAuthenticated]
